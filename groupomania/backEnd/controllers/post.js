02911cbb185ff9d67ae60fb173285686ee697419
Akilai <akilaroha@hotmail.fr>
2021-07-26 14:00:03 +0200

    projet

--- groupomania/backEnd/controllers/post.js
+++ groupomania/backEnd/controllers/post.js
@@ -11,32 +11,67 @@ exports.posts = async (req , res, next) =>{
     const userId = decodedToken.userId;
 
     const postsObject = req.body;
-    console.log(postsObject);
+    console.log(req.body.message)
     var d = new Date();
     var date = d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate();
     console.log(date);
     var d = new Date();
     var hours = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
     console.log(hours);
-    
-    Posts.create({ //Cannot read property 'filename' of undefined
-        'message': postsObject.message, 
-        'date': date,
-        'time':hours,
-        'media': `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-        //(node:13208) UnhandledPromiseRejectionWarning: ReferenceError: userId is not defined
-        'userId' : userId
-    })
-    .then(() => {
-        console.log(userId);
-        //console.log(Posts);
-        res.sendStatus(200);
-    })
-    .catch(error => {
-        console.log(error);
-        res.status(400).json({ error })
-    });
-    
+    if (req.body.message != "") {
+        Posts.create({ //Cannot read property 'filename' of undefined
+            'message': postsObject.message, 
+            'date': date,
+            'time':hours,
+            'userId' : userId
+        })
+        .then(() => {
+            console.log(userId);
+            //console.log(Posts);
+            res.sendStatus(200);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(400).json({ error })
+        });
+    }
+    if(req.file.filename != undefined && req.body.message!="") {
+        Posts.create({ //Cannot read property 'filename' of undefined
+            'message': postsObject.message, 
+            'date': date,
+            'time':hours,
+            'media': `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+            //(node:13208) UnhandledPromiseRejectionWarning: ReferenceError: userId is not defined
+            'userId' : userId
+        })
+        .then(() => {
+            console.log(userId);
+            //console.log(Posts);
+            res.sendStatus(200);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(400).json({ error })
+        });
+    }
+    else if(req.file.filename != undefined && req.body.message=== "") {
+        Posts.create({ //Cannot read property 'filename' of undefined
+            'date': date,
+            'time':hours,
+            'media': `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+            //(node:13208) UnhandledPromiseRejectionWarning: ReferenceError: userId is not defined
+            'userId' : userId
+        })
+        .then(() => {
+            console.log(userId);
+            //console.log(Posts);
+            res.sendStatus(200);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(400).json({ error })
+        });
+    }  
 };
 
 exports.editPost = async (req, res, next) => {
@@ -71,7 +106,6 @@ exports.modifyPost = async (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
     const userId = decodedToken.userId;
-    console.log(req.body);
 
     const handleSuccessfulDeletion = () => {
         res.sendStatus(200);
@@ -79,51 +113,119 @@ exports.modifyPost = async (req, res, next) => {
     const handleError = (error) => {
         error => res.status(500).json({ error })
     }
-    //console.log(req.body.email);
-    let picture = req.body.picture;
-    console.log(picture);
-    Posts.findOne({ where: { id: req.body.postId, userId: userId } })
-    .then(post => {
-        // Check if record exists in db
-        post.update({
-            message: req.body.message,
-            media : `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-        })
-        console.log(post);
-        res.send(post);
-        //en soit ca marche sauf que le post qui change n'est pas celui qui a le bon id
-    })
-    //.then(handleSuccessfulDeletion)
-    .catch(handleError)
+    console.log(req.body)
+    if(userId === userId) {
+        if(req.body.message != "") {
+            //créer une erreur
+            Posts.findOne({ where: { id: req.body.postId, userId: userId} })
+            .then(post => {
+                post.update({
+                    'message': req.body.message
+                })
+                console.log(post);
+                console.log('test1');
+                res.send(post);
+            })
+            .then(handleSuccessfulDeletion)
+            .catch(handleError)
+        }
+        if(req.file.filename != undefined && req.body.message!="") {
+            Posts.findOne({ where: { id: req.body.postId, userId: userId} })
+            .then(post => {
+                post.update({
+                    'message': req.body.message,
+                    'media' : `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+                })
+                console.log(post);
+                console.log('test1');
+                res.send(post);
+            })
+            .then(handleSuccessfulDeletion)
+            .catch(handleError)
+        }
+        else if(req.file.filename != undefined && req.body.message == "") {
+            Posts.findOne({ where: { id: req.body.postId, userId: userId} })
+            .then(post => {
+                post.update({
+                    'media' : `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+                })
+                console.log(post);
+                console.log('test1');
+                res.send(post);
+            })
+            .then(handleSuccessfulDeletion)
+            .catch(handleError)
+        }
+    }
+    else if(userId === 11) {
+        if(req.body.message != "") {
+            Posts.findOne({ where: { id: req.body.postId} })
+            .then(post => {
+                post.update({
+                    'message': req.body.message
+                })
+                console.log(post);
+                console.log('test1');
+                res.send(post);
+            })
+            .then(handleSuccessfulDeletion)
+            .catch(handleError)
+        }
+        if(req.file.filename && req.body.message!="") {
+            Posts.findOne({ where: { id: req.body.postId} })
+            .then(post => {
+                post.update({
+                    'message': req.body.message,
+                    'media' : `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+                })
+                console.log(post);
+                console.log('test1');
+                res.send(post);
+            })
+            .then(handleSuccessfulDeletion)
+            .catch(handleError)
+        }
+        else if(req.file.filename && req.body.message == "") {
+            Posts.findOne({ where: { id: req.body.postId} })
+            .then(post => {
+                post.update({
+                    'media' : `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+                })
+                console.log(post);
+                console.log('test1');
+                res.send(post);
+            })
+            .then(handleSuccessfulDeletion)
+            .catch(handleError)
+        }
+    }
 };
 exports.deletePost = async (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
     const userId = decodedToken.userId;
     console.log(req.body);
-    //la requête est 'pending'
-    //les données envoyées sont
-        //la date, 
-            //l'id du commentaire
-                //le message
-                    //le postId
-                        //le userId
-    //il faut donc faire une recherche où le id  === id
-        //et utiliser la fonction destroy
-
-        //il faut à la suppression du post
-        //supprimer aussi les commentaires qui ont le même postId
-        //on supprime les commentaires qui ont le même postId
+    /**{
+     idpost: {
+        id: 27,
+        message: 'changer le contenu',
+        date: '2021-07-23T22:00:00.000Z',
+        media: 'http://localhost:3030/images/Capture_d’écran_2021-06-22_171015.png1627287298641.png',
+        userId: 10
+    }
+    } */
         console.log('test 2');
         console.log(req.body.idpost.id);
+        //27 === id du post
         console.log('test 2');
         User.findOne({ where: {id: userId} })
         .then(user=> {
             console.log(user.status)
+            //1
             if(user.status === 1 ) {
                 Comment.destroy({
                     where : {
-                        id : req.body.idpost.id
+                        postId : req.body.idpost.id
                     }
                 })
                 //l'id est bien le bon, par contre le commentaire ne se supprime pas et donne null au postId
@@ -138,7 +240,7 @@ exports.deletePost = async (req, res, next) => {
             if(user.status === 0) {
                 Comment.destroy({
                     where : {
-                        id : req.body.idpost.id
+                        postId : req.body.idpost.id
                     }
                 })
                 Posts.destroy({
