21c885a4d295148d53cd064581ef86b593d6686d
Akilai <akilaroha@hotmail.fr>
2021-08-08 21:47:24 +0200

    projet

--- groupomania/src/views/Utilisateurs.vue
+++ groupomania/src/views/Utilisateurs.vue
@@ -2,33 +2,15 @@
   <div class="forum">
     <div class="allMyUtilisateurs">
         <h2>Les utilisateurs Groupomania</h2>
-        <div class="Utilisateurs">
-            <div class="utilisateur" v-for="u of user" :key="u">
-                <div class="user" v-if="u.id === id">
-                    <div class="picture" v-bind:style="{'background-image':'url(' + picture +')'}"></div>
-                    <p>{{ u.name }}</p>
-                    <p>{{ u.lastName }}</p>
-                    <!--<router-link to="/Account" v-on:click="doanything">Account</router-link>-->
-                    <router-link @click.native="moreInfo(u)" v-bind:to="'/accountAdmin/'+ u.id" style="color: white;background-color: cadetblue;padding: 10px 20px;border-radius: 50%;">voir plus</router-link>
-                    <!-- Si le status de l'user est admin alors ajouter un button de suppression de profil -->
-                    <div class="delete" v-if="userId == 11" @click="deleteUser(u.id)">
-                    </div>
+        <div class="Utilisateurs" style="flex-direction: column;">
+            <div class="utilisateur">
+                <div id="user">
                 </div>
-                <div class="pictureOut" v-if="u.id != id">
-                    <div class="pictureNone">
-                        <p>?</p>
-                    </div> 
-                    <p>{{ u.name }}</p>
-                    <p>{{ u.lastName }}</p>
-                    <!--<router-link to="/Account" v-on:click="doanything">Account</router-link>-->
-                    <router-link @click.native="moreInfo(u)" v-bind:to="'/accountAdmin/'+ u.id" style="color: white;background-color: cadetblue;padding: 10px 20px;border-radius: 50%;">voir plus</router-link>
-                    <!-- Si le status de l'user est admin alors ajouter un button de suppression de profil -->
-                    <div class="delete" v-if="userId == 11" @click="deleteUser(u.id)">
-                    </div>
+                <div id="test" style="width: auto;align-items: center;display: flex;justify-content: space-evenly;flex-direction: row;margin: 5%;background-color: cadetblue;padding: 2%;">
                 </div>
             </div>
-        </div>
       </div>
+    </div>
   </div>
 </template>
 
@@ -43,18 +25,133 @@ export default {
             user : null,
             picture : null,
             id : null,
-            userId : null
+            userId : null,
+            status : null,
+            pictureAccount: null,
+            idAccount: null,
+            nameUser: null,
+            lastNameUser: null,
+            accountUserId:null,
+            account: null
         };
     },
     mounted(){
-        /**On veux récupérer tous les users enregistrés dans la bdd */
         axios.get("http://localhost:3030/api/user/all")
-        //récupére les infos présents sur api/account/id
         .then(response => {
-            console.log(response)
+            console.log(response);
             this.user = response.data;
             console.log(this.user);
-            this.userId = localStorage.getItem('userId')
+            this.userId = localStorage.getItem('userId');
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+        axios.get("http://localhost:3030/api/account/all")
+        .then(response => {
+            console.log(response)
+            //affiche les données de l'account 
+            this.account = response.data;
+            console.log(this.account);
+            console.log(response.data);
+            for (let i of this.user) {
+                console.log(i);
+                this.nameUser = i.name;
+                this.lastNameUser = i.lastName;
+                this.accountUserId = i.id;
+                console.log(this.pictureAccount);
+                console.log(i.lastName)
+                console.log(i.id);
+                if(i.accountId === null) {
+                    //si un user a comme valeur accountId null
+                    document.getElementById('user').innerHTML+= `
+                        <div style="margin-right: 20%;">
+                            <div style="height: inherit;width: 150px;background-color: grey;display: flex;justify-content: center;">
+                                <p style="color: white;font-size: 65px;">?</p>
+                            </div>            
+                            <p>${this.nameUser}</p>
+                            <p>${this.lastNameUser}</p>
+                        </div>
+                    `
+                }
+                var btn = document.createElement("BUTTON");   
+                btn.innerHTML += `${this.lastNameUser}`;
+                btn.style.color='white';   
+                btn.style.textAlign= 'center';
+                btn.style.backgroundColor='cadetblue';
+                btn.style.padding='10px 20px';
+                btn.style.borderRadius='50%';
+                btn.style.width='15%';
+                btn.style.border='none';
+                btn.style.textTransform = 'capitalize';     
+                document.getElementById('test').appendChild(btn);
+                btn.addEventListener("click", function() {
+                    window.location.pathname='/accountAdmin/'+ i.id;
+                    console.log('test')
+                    let id = i.id
+                    axios.get(`http://localhost:3030/api/account/${id}`)
+                    .then(function (response) {
+                        console.log(response);
+                        console.log(response.data);
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });   
+                });
+                if(this.status===1) {
+                    var newDiv = document.createElement("div");
+                        document.getElementById('test').appendChild(newDiv);
+                        newDiv.className = 'supAccount';
+                        newDiv.addEventListener("click", function() {
+                            let iduser = i.id;
+                            const monObjet = JSON.parse(localStorage.getItem('token'));
+                            let auth = 'bearer' + " " + monObjet.token;
+                            console.log(auth);
+                            axios.delete("http://localhost:3030/api/user/deleteAnyAccount", {
+                                data: {iduser},
+                                headers: {
+                                    'Authorization': auth
+                                }
+                            }) 
+                            .then(function (response) {
+                                console.log(response);
+                                alert('votre compte a été supprimé');
+                                console.log("l'utilisateur est désinscrit")
+                            })
+                            .then(()=> {
+                                window.location.replace('/Utilisateurs');
+                            })
+                            .catch(function (error) {
+                                console.log(error);
+                            }); 
+                        });
+                }
+            }
+            for (let objt of response.data) {
+                //on boucle sur les composants des lignes dans la table account
+                this.pictureAccount = objt.media;
+                this.idAccount = objt.id;
+                console.log(objt)
+                for (let i of this.user) {
+                    //on boucle sur les données des users
+                    console.log(i);
+                    this.nameUser = i.name;
+                    this.lastNameUser = i.lastName;
+                    this.accountUserId = i.id;
+                    console.log(this.pictureAccount);
+                    console.log(i.lastName)
+                    console.log(i.id);
+                    if(objt.id === i.accountId) {
+                        //<router-link @click.native="moreInfo(user)" v-bind:to="'/accountAdmin/'+ user.id" style="color: white;background-color: cadetblue;padding: 10px 20px;border-radius: 50%; width: 13%;">voir plus</router-link>
+                        document.getElementById('user').innerHTML+= `
+                            <div style="margin-right: 20%;">
+                                <img class="picture" src="${this.pictureAccount}" />
+                                <p>${this.nameUser}</p>
+                                <p>${this.lastNameUser}</p>
+                            </div>
+                        `
+                    }
+                }
+            }
         })
         .catch((error) => {
             console.log(error);
@@ -69,51 +166,29 @@ export default {
             }
         })
         .then(response => {
+            console.log(response);
             console.log(response.data);
             this.picture = response.data.media;
             this.id = response.data.userId;
             console.log(this.picture);
-            console.log(this.userId)
+            console.log(this.userId);
+            console.log(this.id);
         })
         .catch(function (error) {
             this.output = error;
         });
-    },
-    methods: {
-        moreInfo(u) {
-            let id = u.id
-            axios.get(`http://localhost:3030/api/account/${id}`)
-            .then(function (response) {
-                console.log(response);
-                console.log(response.data);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-        },
-        deleteUser(u){
-            let iduser = u;
-            const monObjet = JSON.parse(localStorage.getItem('token'));
-            let auth = 'bearer' + " " + monObjet.token;
-            console.log(auth);
-            axios.delete("http://localhost:3030/api/user/deleteAnyAccount", {
-                data: {iduser},
-                headers: {
-                    'Authorization': auth
-                }
-            }) 
-            .then(function (response) {
-                console.log(response);
-                alert('votre compte a été supprimé');
-                console.log("l'utilisateur est désinscrit")
-            })
-            .then(()=> {
-                window.location.replace('/Utilisateurs');
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+        axios.get("http://localhost:3030/api/user",{
+        headers: {
+          'Authorization': auth
         }
+        })
+        .then(response => {
+            this.status = response.data.status;
+            console.log(this.status);
+        })
+        .catch(function (error) {
+          this.output = error;
+        });
     }
 }
 </script>
@@ -125,20 +200,15 @@ export default {
         display: flex;
         flex-wrap: wrap;
         justify-content: space-evenly;
-        margin-top: 50px;
         .popUp {
             visibility: hidden;
         }
         .utilisateur {
-            height: 200px;
-            width: 150px;
-            display: flex;
-            justify-content: flex-end;
-            flex-direction: column;
-            margin: 10px 70px;
-            .user {
-                height: 200px;
-                width: 150px;
+            #user {
+                display: flex;
+                margin: 5%;
+                flex-direction: row;
+                align-items: center;
                 .picture {
                     display: flex;
                     flex-direction: column;
@@ -189,14 +259,16 @@ export default {
                     }
                 }
             }
-            .delete {
-                background-image: url('./../assets/icones/trash-alt-regular.svg');
-                width: 20px;
-                height:20px;
-                background-repeat: no-repeat;
-                background-size: cover;
-                margin: 15% auto;
-            }
+        }
+        .supAccount {
+            background-image : url('./../assets/icones/trash-alt-regular.svg');
+            width : 20px;
+            height : 20px;
+            background-repeat : no-repeat;
+            background-size : cover;
+            display: flex;
+            filter: invert(1);
+            margin-right: 20%;
         }
     }
 @media all and (min-width: 768px) {
