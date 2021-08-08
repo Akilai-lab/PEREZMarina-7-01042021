7ec67dda314d52e9bdfdd7b855511d38d00693a0
Akilai <akilaroha@hotmail.fr>
2021-08-08 21:46:29 +0200

    projet

--- groupomania/backEnd/routes/account.js
+++ groupomania/backEnd/routes/account.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const users = require('../controllers/account');
+const account = require('../controllers/account');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
-router.get('/:id', users.idShow);
-router.get('/', auth, multer, users.getPicture);
-router.post('/', auth, multer, users.postPicture);
-router.put('/picture', auth, multer, users.putPicture);
-router.put('/', auth, multer,users.postDescription);
+router.get('/all', account.getAllPictures);
+router.get('/:id', account.idShow);
+router.get('/', auth, multer, account.getPicture);
+router.post('/', auth, multer, account.postPicture);
+router.put('/', auth, multer,account.postDescription);
 // router.get('/id/{userId}', users.idShow);
 /*router.post('/api/user', auth, userCtrl.signup);
 //on va créer une route qui va envoyer des données à la bdd pour l'inscription
